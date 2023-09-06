945de2cdc00f6169321198281ac541209a371bce
Jhanvi <jhanvi.ug20@nsut.ac.in>
2023-09-06 11:39:03 +0530

    chunk size increased

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if pdf is not None:
 
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=800,
+        chunk_size=1000,
         chunk_overlap=200,
         length_function=len,)
     texts = text_splitter.split_text(raw_text)
