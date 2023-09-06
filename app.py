ce85f616a70bccbadc6526ac136ddaec4baf8bd7
Jhanvi <jhanvi.ug20@nsut.ac.in>
2023-09-06 11:32:11 +0530

    added cases for no data in pdf

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from langchain.vectorstores import FAISS
 import os
 import pickle
 from langchain.callbacks import get_openai_callback
+
+
 # from dotenv import load_dotenv
 from configEnv import settings
 # load_dotenv()
@@ -36,24 +38,26 @@ if pdf is not None:
     # st.write(texts)
 
     # #vector support
-    store_name = pdf.name[:-4]
-    if os.path.exists(f"{store_name}.pkl"):
-        with open(f"{store_name}.pkl", "rb") as f:
-            doc_search = pickle.load(f)
-        # st.write("Embeddings loaded from disk")
-    else:
-        with open(f"{store_name}.pkl", "wb") as f:
-            embeddings = OpenAIEmbeddings()
-            # st.write(texts)
-            if len(texts) > 0:
-                    doc_search = FAISS.from_texts(texts, embeddings)
-                    pickle.dump(doc_search, f)
+    if len(texts) > 0:
+        store_name = pdf.name[:-4]
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                doc_search = pickle.load(f)
+            # st.write("Embeddings loaded from disk")
+        else:
+            with open(f"{store_name}.pkl", "wb") as f:
+                embeddings = OpenAIEmbeddings()
+                doc_search = FAISS.from_texts(texts, embeddings)
+                pickle.dump(doc_search, f)
             # st.write("Embeddings completion completed")
     query = st.text_input("Ask questions about Pdf file:")
     if query:
-        chain = load_qa_chain(OpenAI(model_name='gpt-3.5-turbo', temperature=0.3), chain_type='stuff')
-        docs = doc_search.similarity_search(query,k=3)
-        with get_openai_callback() as cb:
-             response=chain.run(input_documents=docs, question=query)
-             print(cb)
-        st.write(response)
+        if len(texts) > 0:
+            chain = load_qa_chain(OpenAI(model_name='gpt-3.5-turbo', temperature=0.3), chain_type='stuff')
+            docs = doc_search.similarity_search(query,k=3)
+            with get_openai_callback() as cb:
+                response=chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+        else:
+            st.write('No data extracted from pdf uploaded. Please upload correct pdf.')
