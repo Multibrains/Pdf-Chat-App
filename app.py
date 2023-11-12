af0c3f5d3064206d42792a9a7de25a6eb2137429
Jhanvi <jhanvi.ug20@nsut.ac.in>
2023-11-12 20:52:06 +0530

    added chat history

--- app.py
+++ app.py
@@ -9,54 +9,95 @@ import os
 import pickle
 from langchain.callbacks import get_openai_callback
 import time
-
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from configEnv import settings
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 
-os.environ["OPENAI_API_KEY"] = settings.KEY
-st.title('Pdf Chat App')
-st.header('Chat with PDF')
-pdf = st.file_uploader("Upload your Pdf", type='pdf',
-                       accept_multiple_files=True)
-if pdf is not None:
-    raw_text = ''
-    for single_pdf in pdf: 
-        pdfreader = PdfReader(single_pdf)
-       
-        for i, page in enumerate(pdfreader.pages):
-            content = page.extract_text()
-            if content:
-                raw_text += content
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len,)
-    texts = text_splitter.split_text(raw_text)
 
-    # #vector support
-    if len(texts) > 0:
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def get_vectorstore(texts, pdf):
+    for single_pdf in pdf:
+        store_name = single_pdf.name[:-4]
+
+        if os.path.exists(f"{store_name}.pkl") and os.path.getsize(f"{store_name}.pkl") > 0:
+            with open(f"{store_name}.pkl", "rb") as f:
+                doc_search = pickle.load(f)
+                return doc_search
+        else:
+            with open(f"{store_name}.pkl", "wb") as f:
+                embeddings = OpenAIEmbeddings()
+                doc_search = FAISS.from_texts(texts, embeddings)
+                pickle.dump(doc_search, f)
+                return doc_search
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+
+    os.environ["OPENAI_API_KEY"] = settings.KEY
+    st.write(css, unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.title('Pdf Chat App')
+    st.header('Chat with PDF')
+    pdf = st.file_uploader("Upload your Pdf", type='pdf',
+                           accept_multiple_files=True)
+    if pdf is not None:
+        raw_text = ''
         for single_pdf in pdf:
-            store_name = single_pdf.name[:-4]
+            pdfreader = PdfReader(single_pdf)
 
-            if os.path.exists(f"{store_name}.pkl") and os.path.getsize(f"{store_name}.pkl") > 0:
-                with open(f"{store_name}.pkl", "rb") as f:
-                    doc_search = pickle.load(f)
-            else:
-                with open(f"{store_name}.pkl", "wb") as f:
-                    embeddings = OpenAIEmbeddings()
-                    doc_search = FAISS.from_texts(texts, embeddings)
-                    pickle.dump(doc_search, f)
+            for i, page in enumerate(pdfreader.pages):
+                content = page.extract_text()
+                if content:
+                    raw_text += content
 
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,)
+        texts = text_splitter.split_text(raw_text)
+
+        # #vector support
+    if len(texts) > 0:
+        doc_search = get_vectorstore(texts, pdf)
+        st.session_state.conversation = get_conversation_chain(doc_search)
     query = st.text_input("Ask questions about Pdf file:")
     if query:
         if len(texts) > 0:
-            chain = load_qa_chain(
-                OpenAI(model_name='gpt-3.5-turbo', temperature=0.3), chain_type='stuff')
-            docs = doc_search.similarity_search(query=query, k=3)
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+            handle_userinput(query)
         else:
             st.write(
                 'No data extracted from pdf uploaded. Please upload correct pdf.')
+
+
+if __name__ == '__main__':
+    main()
